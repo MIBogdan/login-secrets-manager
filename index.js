832bdc25e713d9ad2da17b9d914b2d67e3cb0805
Marius <mariusionitabogdan@gmail.com>
2025-03-04 12:43:44 +0200

    update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const bcryptjs = require('bcryptjs');
-const passport = require('passport');
-const Strategy = require('passport-local');
-const session = require('express-session');
-const util = require('util');
-const env = require('dotenv');
-const GoogleStrategy = require('passport-google-oauth2');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const bcryptjs = require("bcryptjs");
+const passport = require("passport");
+const Strategy = require("passport-local");
+const session = require("express-session");
+const util = require("util");
+const env = require("dotenv");
+const GoogleStrategy = require("passport-google-oauth2");
 
 const app = express();
 const port = 3000;
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
+    secret: "secure_key",
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -28,16 +28,15 @@ app.use(
   })
 );
 
-
 app.use(passport.initialize());
 app.use(passport.session());
 
 const db = mysql.createConnection({
-  user: process.env.MYSQLUSER,
-  host: process.env.MYSQL_HOST,
-  database: process.env.MYSQL_DATABASE,
-  password: process.env.MYSQL_PASSWORD,
-  port: process.env.MYSQL_PORT,
+  user: "root",
+  host: "localhost",
+  database: "secrets",
+  password: "bB571174000",
+  port: 3306,
 });
 db.connect();
 
@@ -71,9 +70,9 @@ app.get("/secrets", async (req, res) => {
       const result = await query(`SELECT secret FROM users WHERE email = '${userEmail}'`);
       const secret = result[0].secret;
       if (secret) {
-        res.render("secrets.ejs", {secret: secret});
+        res.render("secrets.ejs", { secret: secret });
       } else {
-        res.render("secrets.ejs", {secret: "You don't have any secrets. Submit one!"});
+        res.render("secrets.ejs", { secret: "You don't have any secrets. Submit one!" });
       }
     } catch (err) {
       res.send(err);
@@ -93,31 +92,20 @@ app.get("/submit", (req, res) => {
 
 app.post("/submit", async (req, res) => {
   const userSecret = req.body.secret;
-  const email = req.user.email
+  const email = req.user.email;
   try {
     await query(`UPDATE users SET secret = '${userSecret}' WHERE email = '${email}'`);
     res.redirect("/secrets");
   } catch (err) {
     res.send(err);
   }
-  
 });
-
-app.get("/auth/google", passport.authenticate("google", {
-  scope: ["profile", "email"],
-}));
-
-app.get("/auth/google/secrets", passport.authenticate("google", {
-  successRedirect: "/secrets",
-  failureRedirect: "/login",
-})
-);
 
 app.get("/logout", (req, res) => {
   req.logout((err) => {
     if (err) console.log(err);
     res.redirect("/");
-  })
+  });
 });
 
 app.post(
@@ -136,18 +124,17 @@ app.post("/register", async (req, res) => {
     const checkResult = await query(`SELECT * FROM users WHERE email = '${email}'`);
 
     if (checkResult.length > 0) {
-      req.redirect("/login");
+      res.redirect("/login");
     } else {
       bcryptjs.hash(password, saltRounds, async (err, hash) => {
         if (err) {
           console.error("Error hashing password:", err);
         } else {
-          const result = await query(
-            `INSERT INTO users (email, password) VALUES ('${email}', '${hash}') RETURNING *`);
+          const result = await query(`INSERT INTO users (email, password, secret) VALUES ('${email}', '${hash}', "")`);
           const user = result[0];
           req.login(user, (err) => {
             console.log("success");
-            res.redirect("/secrets");
+            res.redirect("/login");
           });
         }
       });
@@ -157,7 +144,8 @@ app.post("/register", async (req, res) => {
   }
 });
 
-passport.use("local",
+passport.use(
+  "local",
   new Strategy(async function verify(username, password, cb) {
     try {
       const result = await query(`SELECT * FROM users WHERE email = '${username}'`);
@@ -187,28 +175,6 @@ passport.use("local",
     }
   })
 );
-
-passport.use("google", new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets",
-  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-}, async (accessToken, refreshToken, profile, cb) => {
-  // console.log(profile);
-
-  try {
-    const result = await query(`SELECT * FROM users WHERE email = '${profile.email}'`);
-    if (result.length === 0) {
-      const newUser = await query(`INSERT INTO users (email, password) VALUES ('${profile.email}', 'google')`)
-      cb(null, newUser[0])
-    } else {
-      cb(null, result[0])
-    }
-  } catch (err) {
-    cb(err);
-  }
-}
-))
 
 passport.serializeUser((user, cb) => {
   cb(null, user);
